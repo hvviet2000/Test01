d18fbf7f357b8c918db73e105ff7fa8a1a457315
hvviet2000 <hvviet2000@yahoo.com>
2024-04-11 17:11:28 +0700

    Add files via upload

---
+++ crypt.bat
@@ -0,0 +1,11 @@
+net start cryptsvc
+regsvr32 softpub.dll
+regsvr32 wintrust.dll
+regsvr32 initpki.dll
+regsvr32 dssenh.dll
+regsvr32 rsaenh.dll
+regsvr32 gpkcsp.dll
+regsvr32 sccbase.dll
+regsvr32 slbcsp.dll
+regsvr32 cryptdlg.dll
+pause
